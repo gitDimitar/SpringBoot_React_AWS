4f858ffadaf101982c8db2b49678b843bf6d2583
gitDimitar <dpapazikov@gmail.com>
2021-01-23 14:23:39 +0000

    basic display and accept files into front end
    Front-end still only one directional from backend. Consumes only GET endpoint of user.

--- src/main/my-app/src/App.js
+++ src/main/my-app/src/App.js
@@ -1,23 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import {useDropzone} from 'react-dropzone'
+import React, {useState, useEffect, useCallback} from "react";
+
+const Users = () => {
+  const [users, setUsers] = useState([]);
+
+const fetchUsers = () => {
+
+  axios.get("http://localhost:8080/api/user").then(res => {
+    console.log(res);
+    setUsers(res.data);
+  });
+};
+
+useEffect(() => {
+  fetchUsers();
+}, []);
+
+return users.map((user, index) => {
+  return (
+  <div key={index}>
+    {/* TODO: put image here */}
+    <br/>
+    <br/>
+    <h1>{user.userName}</h1>
+    <p>{user.userId}</p>
+    <Dropzone />
+    <br/>
+  </div>
+  )
+});
+
+};
+
+function Dropzone() {
+  const onDrop = useCallback(acceptedFiles => {
+    // Do something with the files
+    const file = acceptedFiles[0];
+    console.log(file);
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+          <p>Drop the image here ...</p> :
+          <p class="imageUploader">Click here to upload user image.</p>
+      }
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Users/>
     </div>
   );
 }
